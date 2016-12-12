e513952dadf1c572576ca25a0d6ba6cbebae2847
Adrien HEG <Adrien HEG>
2016-12-12 15:38:56 +0100

    debug index

--- www/js/listeJeux.js
+++ www/js/listeJeux.js
@@ -4,9 +4,9 @@ function getAllGames() {
         .then(function (response) {
             $.each(response,function() {
                $("#listViewJeux").append("<a class='list-group-item allow-badge widget uib_w_13' data-uib='twitter%20bootstrap/list_item' data-ver='1'><p class='list-group-item-text'><img width='70px'  style='float : left' src='"+$(this)[0].featured_image_thumbnail_url+"' /><h4 class='list-group-item-heading'>"+$(this)[0].title.rendered+"</h4></p></a>");
-            });
+            })
         })
-};
+}
 
 
 function getOneGames(id) {
@@ -15,7 +15,6 @@ function getOneGames(id) {
         .then(function (response) {
             return respond.data;
         })
-};
-
+}
 
 getAllGames();
